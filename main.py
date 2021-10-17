fbb0967ac07f1f4e96cc66636a7fd45d0210faca
AzureAD\KlaudiaBrzezinska <kb@ds-s.at>
2021-10-17 10:13:56 +0200

    Data Frame for Data prepared

--- main.py
+++ main.py
@@ -2,47 +2,19 @@ import requests
 import matplotlib.pyplot as plt
 import json
 import pandas as pd
-from datetime import datetime
+
 response = requests.get("https://api.nasa.gov/DONKI/FLR?startDate=2021-09-01&endDate=2021-10-01&api_key=zSbjNFJgVZ45n9AZcGLzR9GiNx2C8Ttd50oOf0hA")
 json_data = response.json()
 print(json_data)
 
-#TEST TEST TEST
-
-items = json_data[:]
-active_region_num_arr = []
-begin_time_arr = []
-date_time_obj_arr = []
-sourceLocation_arr = []
-classType_arr = []
-for item in items:
-	flrID = item['activeRegionNum']
-	active_region_num = item['beginTime']
-	date_time_str = item['beginTime']
-	date_time_obj = datetime.fromisoformat(date_time_str[:-1])
-
-	sourceLocation = item['sourceLocation']
-	classType = item['classType']
-
-	active_region_num_arr.append(flrID)
-	begin_time_arr.append(active_region_num)
-	date_time_obj_arr.append(date_time_obj)
-
-	sourceLocation_arr.append(sourceLocation)
-	classType_arr.append(classType)
-	#print(flrID)
-
 df = pd.json_normalize(json_data)
-print(df)
-print(df.classType)
-print(df.sourceLocation)
-#print(active_region_num_arr)
-#print(begin_time_arr)
-#print(date_time_obj_arr)
-#print(sourceLocation_arr)
-#print(classType_arr)
-plt.scatter(df.classType, df.sourceLocation)
-plt.ylabel('sourceLocation')
-plt.xlabel('activeRegionNum')
-plt.show()
+df['duringTime'] = (pd.to_datetime(df.endTime) - pd.to_datetime(df.beginTime))
+df['duringTimeHighPoint'] = (pd.to_datetime(df.peakTime) - pd.to_datetime(df.beginTime))
+print(df['duringTime'])
+
+data_nasa = [df["flrID"], df["duringTime"], df["duringTimeHighPoint"], df["classType"], df["sourceLocation"], df["activeRegionNum"]] #create data
+data_frame_nasa = pd.concat(data_nasa, axis=1) #create data frame from data
+print(data_frame_nasa)
+print(data_frame_nasa.duringTimeHighPoint)
+print(data_frame_nasa.duringTime)
 
