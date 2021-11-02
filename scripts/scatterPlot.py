b2c7e21c63f747cf222b2b985deb56198cb468d8
klaudidaBrzezinska <brzezinska.klaudia93@gmail.com>
2021-11-02 22:17:40 +0100

    add legend for color fill

--- scripts/scatterPlot.py
+++ scripts/scatterPlot.py
@@ -8,13 +8,13 @@ def scatterPlot_tab(data_frame_nasa):
     # the data
     x = data_frame_nasa['duringTime']
     y = data_frame_nasa['month']
-    index_cmap = factor_cmap('class_type_simple', palette=['red', 'blue', 'green', 'pink'],
-                             factors=sorted(data_frame_nasa['class_type_simple']))
+    index_cmap = factor_cmap('class_type_simple', palette=['red', 'blue', 'green', 'yellow'],
+                             factors=sorted(data_frame_nasa['class_type_simple'].unique()))
     # determine best fit line
     p = figure(plot_width=600, plot_height=450, title="During Time for every month", toolbar_location=None,
                tools="hover")
     p.scatter('duringTime', 'month', source=data_frame_nasa, fill_color=index_cmap, fill_alpha=0.6,  size=10,
-              legend='Class Type')
+              legend='class_type_simple')
     p.xaxis.axis_label = 'Minutes'
     p.yaxis.axis_label = 'Month'
     p.legend.location = "top_left"
